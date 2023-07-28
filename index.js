a959550a3f76ae739fed78242369f315585bdb46
unicron2 <132698393+unicron2@users.noreply.github.com>
2023-07-28 13:23:25 +0000

    update

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";      // Requisição do pacote do express
+const app = express();              // Instancia o Express
+const port = 3000;                  // Define a porta
+
+app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
+  res.json({
+    nome: "João Marcos Martins Xavier",      // Substitua pelo seu nome
+  });
+  console.log("Rota / solicitada");
+});
+
+app.listen(port, () => {            // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
